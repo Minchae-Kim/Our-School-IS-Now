a1c2b81facee1e83b2bb58c9f7c720cf028d30d4
kim-su-hwan <64351122+kim-su-hwan@users.noreply.github.com>
2022-05-09 19:38:14 +0900

    Make Scene
    singleton and make scene

--- Assets/Scripts/SceneScripts/StartSceneManager.cs
+++ Assets/Scripts/SceneScripts/StartSceneManager.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-public class ScencScripts : MonoBehaviour
+using UnityEngine.SceneManagement;
+
+public class StartSceneManager : MonoBehaviour
 {
+    private static StartSceneManager instance = null;
+    public static StartSceneManager Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(instance)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+    
+    public void OnGameStart()
+    {
+        SceneManager.LoadScene("SchoolSceneDayMobile_test");
     }
 
-    // Update is called once per frame
-    void Update()
+
+    public void OnGameQuitButton()
     {
-        
+        Application.Quit();
     }
+
 }
