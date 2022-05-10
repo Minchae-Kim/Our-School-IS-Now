53194329a2ac7678e8852863dd40674843009eba
Minchae Kim <yjk199900@gmail.com>
2022-05-10 16:50:06 +0900

    소화기, 불 추가
    UI 글씨 깨지는 문제 수정
    소화기 타입 선택 가능
    강당, 화장실에 소화기 추가 배치

--- Assets/Scripts/Fire/FireExtinguisher.cs
+++ Assets/Scripts/Fire/FireExtinguisher.cs
@@ -8,11 +8,17 @@ public class FireExtinguisher : MonoBehaviour
     [SerializeField] private GameObject hose1;
     [SerializeField] private GameObject hose2;
     [SerializeField] private GameObject effect;
+    [SerializeField] private GameObject menu;
 
     private bool isPinRemoved = false;
 
     private Animator animator;
     private ParticleSystem ps;
+
+    private int type;   // 소화기 종류: 0-A급, 1-B급, 2-C급
+    public int Type => type;
+
+   private bool isSelected = false;
 
     private void Awake()
     {
@@ -20,6 +26,11 @@ public class FireExtinguisher : MonoBehaviour
         ps = effect.GetComponent<ParticleSystem>();
     }
 
+    public void ShowMenu()
+    {
+        if (!isSelected)
+            menu.SetActive(true);
+    }
 
     public void RemovePin()
     {
@@ -80,5 +91,28 @@ public class FireExtinguisher : MonoBehaviour
         yield return new WaitForSeconds(2);
         hose1.SetActive(false);
         hose2.SetActive(true);
+    }
+
+    
+    // menu
+    public void OnSelectAtype()
+    {
+        type = 0;
+        isSelected = true;
+        menu.SetActive(false);
+    }
+
+    public void OnSelectBtype()
+    {
+        type = 1;
+        isSelected = true;
+        menu.SetActive(false);
+    }
+
+    public void OnSelectCtype()
+    {
+        type = 2;
+        isSelected = true;
+        menu.SetActive(false);
     }
 }
