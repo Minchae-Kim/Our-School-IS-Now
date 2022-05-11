44ac201ea9b37d1d6fa4b1216805caff9d5107c5
kim-su-hwan <64351122+kim-su-hwan@users.noreply.github.com>
2022-05-11 18:28:34 +0900

    Using UnityEvent
    asdadfasdfadf

--- Assets/Scripts/Fire/FireParent.cs
+++ Assets/Scripts/Fire/FireParent.cs
@@ -1,13 +1,40 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FireParent : MonoBehaviour
 {
+    public UnityEvent offFire;
+    private GameObject[] fires = new GameObject[2];
 
-    public bool IsFire()
+    private void Awake()
     {
+        fires[0] = transform.GetChild(0).gameObject;
+        fires[1] = transform.GetChild(1).gameObject;
+    }
+
+    private void Update()
+    {
+        if(!isFire())
+        {
+            if (offFire == null)
+                Debug.Log("불꺼짐 이벤트에 아무것도 없습니다.");
+            else
+            {
+                offFire.Invoke();
+                this.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private bool isFire()
+    {
+        if (!fires[0].activeSelf && !fires[1].activeSelf)
+            return false;
         return true;
     }
+
+
 
 }
--- Assets/Scripts/UIScripts/UIManager.cs
+++ Assets/Scripts/UIScripts/UIManager.cs
@@ -2,25 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class UIManager : MonoBehaviour
 {
     private static UIManager s_instance;
 
+    [SerializeField] private TMP_Text fireCountText;
+    private int nowFireCount = 5;
     //화살표
-    [SerializeField] private GameObject explainArrow;
+    //[SerializeField] private GameObject explainArrow;
 
     //시작 ui, 물수건 제작 UI, 화재 종류 설명 ui
     //[SerializeField] private GameObject startUI;
     //[SerializeField] private GameObject towelUI;
     //[SerializeField] private GameObject fireTypeUI;
 
-
-
     private void Awake() => s_instance = this;
     private void Start()
     {
-        
+
     }
 
     public void Show(GameObject UI)
@@ -33,6 +34,15 @@ public class UIManager : MonoBehaviour
         UI.SetActive(false);
     }
 
+    public void ReduceFireCont()
+    {
+        if (nowFireCount > 0)
+            nowFireCount--;
+    }
 
+    public void RenewalFireText()
+    {
+        fireCountText.text = $"Remain Fire : {nowFireCount}";
+    }
 
 }
