55a9f828aa7e70828df4d7e4fd2029663061e852
kim-su-hwan <64351122+kim-su-hwan@users.noreply.github.com>
2022-05-11 15:30:56 +0900

    Add fire Event
    asdfadfadf

--- Assets/Scripts/Fire/FireParent.cs
+++ Assets/Scripts/Fire/FireParent.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class FireParent : MonoBehaviour
 {
+    public UnityEvent onFireOffUI = null;
+
+    GameObject[] fires = new GameObject[2];
+
+    private void Awake()
+    {
+        fires[0] = transform.GetChild(0).gameObject;
+        fires[1] = transform.GetChild(1).gameObject;
+    }
 
     public bool IsFire()
     {
+        if (fires[0].activeSelf == false && fires[1].activeSelf == false)
+        {
+            if (onFireOffUI == null)
+                Debug.Log("화재 발생 이벤트 함수가 추가가 안됨");
+            else
+                onFireOffUI.Invoke();
+            return false;
+        }
         return true;
     }
+
 
 }
--- Assets/Scripts/UIScripts/UIManager.cs
+++ Assets/Scripts/UIScripts/UIManager.cs
@@ -7,20 +7,24 @@ public class UIManager : MonoBehaviour
 {
     private static UIManager s_instance;
 
+    //남은 화재 갯수 UI
+    [SerializeField] private Text FireText;
     //화살표
-    [SerializeField] private GameObject explainArrow;
+    //[SerializeField] private GameObject explainArrow;
 
     //시작 ui, 물수건 제작 UI, 화재 종류 설명 ui
     //[SerializeField] private GameObject startUI;
     //[SerializeField] private GameObject towelUI;
     //[SerializeField] private GameObject fireTypeUI;
 
+    //화재 갯수 UI
+    private int fireCount = 5;
 
 
     private void Awake() => s_instance = this;
     private void Start()
     {
-        
+        FireText.text = $"Reamain Fire  : {fireCount}";
     }
 
     public void Show(GameObject UI)
@@ -33,6 +37,16 @@ public class UIManager : MonoBehaviour
         UI.SetActive(false);
     }
 
+    public void FireCountDown()
+    {
+        if (fireCount != 0)
+            fireCount--;
+    }
+
+    public void RenewalFireCountDown()
+    {
+        FireText.text = $"Reamain Fire  : {fireCount}";
+    }
 
 
 }
